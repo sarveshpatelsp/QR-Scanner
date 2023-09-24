82c6a79958e63230ae9752a690f4867064b53b7c
Sarvesh Patel <sarveshpatelsp91@gmail.com>
2023-09-24 20:01:06 +0530

    Added the splash screen and textView in Scanner activity_scanner.xml

---
+++ app/src/main/java/com/example/qrscanner/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.example.qrscanner
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_DELAY: Long = 2000 // 2 seconds
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // Delay for the specified duration and then start the main activity
+        Handler().postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_DELAY)
+    }
+}
